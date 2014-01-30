582e4e9c5dc9faa8d738563ab49242f9199bf64d
David Cadavid" <cadavid219@gmail.com>
2014-01-29 20:10:40 -0530

    LIMPIEZA GENERAL

--- valyrianFirstGame/src/com/valyrian/firstgame/utilitarios/ManejadorUnidades.java
+++
@@ -1,6 +0,0 @@
-package com.valyrian.firstgame.utilitarios;
-
-public class  ManejadorUnidades {
-public static final int PIXELSTOMETERS=32; 	//32 pixeles, equivalen a 1 metro
-										// esto es para efectos de las fisicas en Box2D
-}
